321630f9b2f616f2918af967aad89dd7d8f5af9d
kgullanat <30823949+kgullanat@users.noreply.github.com>
2017-08-08 16:14:59 +0700

    Initial commit

---
+++ src/Duck.java
@@ -0,0 +1,18 @@
+
+public abstract class Duck {
+	Flybehavior flybehavior;//ตัวแปรคลาส Flybehavior
+	Quackbehavior quackbehavior;//ตัวแปรคลาส Quackbehavior
+	void performQuack(){
+		quackbehavior.quack();
+	}
+	void setQuackBehavior (Quackbehavior qb){
+		quackbehavior = qb;
+	}
+	void performFly(){
+		flybehavior.fly();
+	}
+	void swim(){
+		System.out.println("I'm swimming");
+	}
+	abstract void display();
+}
